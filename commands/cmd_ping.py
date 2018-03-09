9771f8d9c6834f3477986c6d2e8794a2d528b3ff
Florian Lödermann <floedermann@gmail.com>
2018-03-09 18:29:58 +0100

    Updated Pings

--- commands/cmd_ping.py
+++ commands/cmd_ping.py
@@ -6,5 +6,5 @@ def ex(args, message, client, invoke):
     args_out = ""
     if len(args) > 0:
         args_out = "\n\n__**Coalition-Ping**__ \n%s" % args.__str__()[1:-1].replace("'", "").replace(",", "")
-
-        yield from client.send_message(client.get_server("").get_channel(""), "@everyone" + args_out)
+    for s in client.servers:
+        client.send_message(client.get_server(s).get_channel("1234"), "@everyone" + args_out)
